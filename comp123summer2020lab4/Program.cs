b7f1432aa39db60653142c9ebe74a2c408de8c0f
Richard Elridge <richardelridge78@outlook.com>
2020-06-19 15:42:06 -0700

    I added the person class

---
+++ comp123summer2020lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace comp123summer2020lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
