89bfdcc01e3c13120ab56a35e878f7df836931c4
Richard Elridge <richardelridge78@outlook.com>
2020-06-19 15:43:19 -0700

    I created the person class

---
+++ comp123summer2020lab4/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace comp123summer2020lab4
+{
+    class Person
+    {
+        // Private Instance Varible
+
+        // Public Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //contrctors
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        //public method
+        public void sayhello()
+        {
+            Console.WriteLine($"{Name} say hello to you");
+        }
+    }
+}
